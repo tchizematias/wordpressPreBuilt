fc54b73f6a96eca4baab6c099993a7c2d691dbaa
tdlmatias <tchize7@gmail.com>
2017-06-11 20:56:28 +0100

    Correcting errors on install script

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
+
+#Update your systems
 sudo apt-get update
+sudo apt-get upgrade -y
 
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-sudo apt-get install -y vim curl python-software-properties
-sudo add-apt-repository -y ppa:ondrej/php5
+# Install all pre-dependencies
+sudo apt-get install -y vim curl lynx screen nmap 
+sudo apt-get install -y python3 python3-pipy python-software-properties
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+
+# Add Ondrej Repository with PHP5
+sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
 
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core php5-xdebug
+sudo apt-get install -y php5.6 apache2 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mcrypt php5.6-readline mysql-server-5.5 php5.6-mysql git-core php5.6-xdebug
 
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
 xdebug.scream=1
@@ -17,9 +26,9 @@ EOF
 
 sudo a2enmod rewrite
 
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5.6/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5.6/apache2/php.ini
+sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5.6/cli/php.ini
 
 sudo service apache2 restart
 
